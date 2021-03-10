9f09ca0630d26bb25f5f6e987937823768919110
Glitch (impossible-meteor-blackcurrant) <none>
2021-03-10 12:26:36 +0000

    🌉⛰ Checkpoint ./commands/info.js:29551264/168

--- commands/info.js
+++ commands/info.js
@@ -8,19 +8,18 @@ module.exports.run = async (client, message, args) => {
 const prefix = process.env.PREFIX;
 const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
 let embed = new Discord.RichEmbed()
-.setTitle(`Information for developers`)
+.setTitle(`Akemiツ`)
 .setColor('RANDOM')
 .setDescription(`My prefix is: \`${prefix}\`\n`)
 .setThumbnail(client.user.displayAvatarURL)
-.addField('• Developer', `${cnf.owner}`)
-.addField('• Node', `${process.version}`)
+.addField('• Developer', `Sakshyam`)
 .addField('• Uptime', `${duration}`)
 .addField('• Guild Count', `${client.guilds.size}`)
 .addField('• User Count', `${client.users.size}`)
 .addField('• Channel Count', `${client.channels.size}`)
 .addField('• Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`)
-.addField('• Useful Links', `[Official website]() | [Invite me!](https://discord.com/oauth2/authorize/?permissions=8&scope=bot&client_id=${client.user.id}) | [Official server](https://discord.gg/b44VXcVccg)`)
-.setFooter(`Bot created by ${cnf.owner}`)
+.addField('• Useful Links', `[Official website](https://discordbotlist.com/bots/akemi-0216) | [Invite me!](https://discord.com/oauth2/authorize/?permissions=8&scope=bot&client_id=${client.user.id}) | [Official server](https://discord.gg/b44VXcVccg)`)
+.setFooter(`Bot created by Sakshyam`)
 message.channel.send(embed);
 }
 
