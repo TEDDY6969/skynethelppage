9cce5001c1519245f3572227aa79ee43682f6573
Glitch (impossible-meteor-blackcurrant) <none>
2021-03-11 10:14:30 +0000

    🎱🎗 Checkpoint ./commands/invite.js:29551264/360

--- commands/invite.js
+++ commands/invite.js
@@ -4,18 +4,17 @@ const cnf = require('../config.json');
 module.exports.run = async (client, message, args) => {
 try {
 const embed = new Discord.RichEmbed()
-  .setTitle("Invite The Bot ")
+  .setTitle("Invite SkyNet ツ  ")
   .setColor('RANDOM')
-  .addField("Invite to Discord server", "[Invite the bot here (Recomended!)](" + `https://discord.com/api/oauth2/authorize?client_id=819172557067386891&permissions=8&scope=bot` + ") \n[Invite The Bot Here (Normal)](https://discord.com/api/oauth2/authorize?client_id=819172557067386891&permissions=8&scope=bot" + `${client.user.id})`)
+  .addField("Invite Our Bot ", "[Invite the bot here (Recomended!)](" + `https://discord.com/api/oauth2/authorize?client_id=819172557067386891&permissions=8&scope=bot` + ") \n[Invite The Bot Here (Normal)](https://discord.com/api/oauth2/authorize?client_id=819172557067386891&permissions=8&scope=bot" + `${client.user.id})`)
   .addField("Website", `[Visit webiste](https://discordbotlist.com/bots/akemi-0216)`)
   .addField("Server", `[Join to official server](https://discord.gg/b44VXcVccg)`)
-  .setFooter("Bot created by " + `Sakshyam`)
   .setTimestamp()
 message.author.send({embed})
 
 message.channel.send({embed: {
             color: 3447003,
-            description: "Check a priv message!"
+            description: "Check Your Dm !"
         }})
 } catch (err) {
     message.channel.send({embed: {
