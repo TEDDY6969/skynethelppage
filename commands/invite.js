bb0388e87686ac62a61710747c41ff89a693d7e1
Glitch (impossible-meteor-blackcurrant) <none>
2021-03-11 16:30:20 +0000

    🍮👠 Checkpoint ./commands/invite.js:29551264/131

--- commands/invite.js
+++ commands/invite.js
@@ -4,9 +4,9 @@ const cnf = require('../config.json');
 module.exports.run = async (client, message, args) => {
 try {
 const embed = new Discord.RichEmbed()
-  .setTitle("Invite SkyNet ツ  <a:Hearth:819514793303212062> ")
+  .setTitle(" <a:Hearth:819514793303212062> Invite SkyNet ツ  ")
   .setColor('RANDOM')
-  .addField("``Links``  <a:redBadge:819501788766928927> ", "[Invite the bot here (Recomended!)](" + `https://discord.com/api/oauth2/authorize?client_id=819172557067386891&permissions=8&scope=bot` + ") \n[ Invite The Bot Here (Normal)](https://discord.com/api/oauth2/authorize?client_id=819172557067386891&permissions=8&scope=bot" + `${client.user.id})`)
+  .addField(" <a:redBadge:819501788766928927> Links  ", "[Invite the bot here (Recomended!)](" + `https://discord.com/api/oauth2/authorize?client_id=819172557067386891&permissions=8&scope=bot` + ") \n[Invite The Bot Here (Normal)](https://discord.com/api/oauth2/authorize?client_id=819172557067386891&permissions=8&scope=bot" + `${client.user.id})`)
   .addField("Website", `[ Visit Webiste](https://discordbotlist.com/bots/akemi-0216)`)
   .addField("Server", `[Join to official server](https://discord.gg/b44VXcVccg)`)
   .setTimestamp()
